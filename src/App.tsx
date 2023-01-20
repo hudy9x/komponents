dd950b6417ca8ab4df37751d24d039cb5cf52bf4
hudy9x <hudy9x@gmail.com>
2023-01-20 17:28:45 +0700

    feat: create confirmbox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { confirmAlert, confirmWarning } from "./components/Confirmbox"
 import { messageError, messageInfo, messageSuccess, messageWarning } from "./components/Message"
 
 function App() {
@@ -7,13 +8,31 @@ function App() {
   const onError = () => { messageError("Ooops! there some errors here") }
   const onWarning = () => { messageWarning("Make sure that your double check this action") }
 
+  const onConfirmWarning = () => {
+    confirmWarning({
+      message: "This action may affect to the operation of other users. Do you still want to continue doing this ?",
+      yes: () => { messageSuccess("OK, this action is executed!") },
+      no: () => { messageError("You cancelled this action") }
+    })
+  }
+  const onConfirmAlert = () => {
+    confirmAlert({
+      message: "Are you sure you want to delete this document?. Remember that this action will delete the document permanently.",
+      yes: () => { messageSuccess("Document has been deleted !") }
+    })
+  }
+
   return (
-    <div id="wrapper" className="h-screen bg-gray-200 flex items-center justify-center" >
+    <div id="wrapper" className="h-screen bg-gray-200 flex flex-col gap-6 items-center justify-center" >
       <div className="flex items-center gap-8">
+        <button onClick={onConfirmWarning} className="btn-warning" >Confirm warning</button>
+        <button onClick={onConfirmAlert} className="btn-danger" >Confirm alert</button>
+      </div>
+      <div className="hidden items-center gap-8">
         <button onClick={onInfo} >Info</button>
-        <button onClick={onSuccess} className="bg-indigo-600 text-white" >Success</button>
-        <button onClick={onError} className="bg-red-400 text-white" >Alert</button>
-        <button onClick={onWarning} className="bg-yellow-400 text-white" >Warning</button>
+        <button onClick={onSuccess} className="btn-primary" >Success</button>
+        <button onClick={onError} className="btn-danger" >Alert</button>
+        <button onClick={onWarning} className="btn-warning" >Warning</button>
       </div>
     </div>
   );
---
+++ src/components/Confirmbox/ConfirmBox.tsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react"
+import { Root } from "react-dom/client"
+import { getConfirmIcons } from "./Icons"
+import { ConfirmAction, ConfirmboxType } from "./type"
+
+interface Props {
+  type: ConfirmboxType,
+  action: ConfirmAction,
+  root: Root,
+  container: HTMLDivElement
+}
+
+export default function ConfirmBox({type, action, root, container}: Props) {
+  const [visible, setVisible] = useState(true)
+  const {message, yes, no} = action
+
+  const close = () => {
+    container.style.display = "none"
+    setVisible(false)
+
+    setTimeout(() => {
+      root.unmount()
+      container.remove()
+    }, 1000);
+    
+  }
+
+  const onOk = () => {
+    yes()
+    close()
+  }
+
+  const onCancel = () => {
+    no && no()
+    close()
+  }
+
+  useEffect(() => {
+    const clickOutsideHandler = (ev: MouseEvent) => {
+      const target = ev.target as HTMLElement;
+
+      if (target.classList.contains('confirmbox') || target.closest('.confirmbox')) {
+        return;
+      }
+
+      close()
+    }
+
+    container.addEventListener("click", clickOutsideHandler);
+    
+    return () => {
+      container.removeEventListener("click", clickOutsideHandler)
+    }
+
+  })
+
+  return <div className={`confirmbox ${type} ${visible ? "" : "hidden"}`} >
+    {getConfirmIcons(type)}
+    <p>{message}</p>
+    <div className="confirm-actions grid grid-cols-2 gap-4 w-full">
+      <button className="btn-primary btn-sm" onClick={onOk}>Ok</button>
+      <button className="btn-sm" onClick={onCancel}>Cancel</button>
+    </div>
+  </div>
+}
