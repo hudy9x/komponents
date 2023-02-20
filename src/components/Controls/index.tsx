9968906099df0e05a75e1bdf31b8533fcd88ebcb
hudy9x <hudy9x@gmail.com>
2023-02-21 00:33:17 +0700

    feat: add control list

--- src/components/Controls/index.tsx
+++ src/components/Controls/index.tsx
@@ -1,5 +1,7 @@
 import InputControl from "./InputControl"
 import TextareaControl from "./TextareaControl"
+import ListControl from "./ListControl"
 
 export const Input = InputControl
 export const Textarea = TextareaControl
+export const List = ListControl
