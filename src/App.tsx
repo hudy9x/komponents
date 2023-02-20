01405d0d7ba9e45c9a96aa13830df6ef8dbf54e9
hudy9x <hudy9x@gmail.com>
2023-02-21 00:37:20 +0700

    remove unused code in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,6 @@
-import { EnvelopeIcon } from "./assets/EnvelopIcon"
-import { confirmAlert, confirmWarning } from "./components/Confirmbox"
-import { Input, Textarea } from "./components/Controls"
-import { messageError, messageInfo, messageSuccess, messageWarning } from "./components/Message"
-import FormContainer from "./containers/FormContainer"
 import ListContainer from "./containers/ListContainer"
 
 function App() {
-
-  const onInfo = () => { messageInfo("Displays some information") }
-  const onSuccess = () => { messageSuccess("Yep! the document has been created !") }
-  const onError = () => { messageError("Ooops! there some errors here") }
-  const onWarning = () => { messageWarning("Make sure that your double check this action") }
-
-  const onConfirmWarning = () => {
-    confirmWarning({
-      message: "This action may affect to the operation of other users. Do you still want to continue doing this ?",
-      yes: () => { messageSuccess("OK, this action is executed!") },
-      no: () => { messageError("You cancelled this action") }
-    })
-  }
-  const onConfirmAlert = () => {
-    confirmAlert({
-      message: "Are you sure you want to delete this document?. Remember that this action will delete the document permanently.",
-      yes: () => { messageSuccess("Document has been deleted !") }
-    })
-  }
-
   return (
     <div id="wrapper" className="h-screen bg-gray-200 flex flex-col gap-6 items-center justify-center" >
       <ListContainer />
